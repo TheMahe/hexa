7646f010b0e8fad876120a4491c5b8e38b2faf5d
Madevelopment <mahir.delic.07@gmail.com>
2024-01-08 21:33:59 +0100

    added edit account functionality

--- js/hexa.js
+++ js/hexa.js
@@ -10,6 +10,9 @@ if(session_id !== "") {
 
         document.querySelector('#username').innerHTML = user.username;
         document.querySelector('#email').innerHTML = user.email;
+
+        document.querySelector('#korisnicko_ime').value = user.username;
+        document.querySelector('#edit_email').value = user.email;
     }
 
     populateUserData()
@@ -24,4 +27,22 @@ document.querySelector('#logout').addEventListener('click', function(e) {
     let session = new Session();
     session.destroySession();
     window.location.href = '/';
+})
+
+document.querySelector('#editAccount').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.edit-acc-modal').style.display = 'block';
+})
+
+document.querySelector('#close-acc-modal').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.edit-acc-modal').style.display = 'none';
+})
+
+document.querySelector('#editForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let user = new User();
+    user.username = document.querySelector('#korisnicko_ime').value;
+    user.email = document.querySelector('#edit_email').value;
+    user.edit();
 })
