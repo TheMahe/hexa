1ced6eea1a943316daaaf2ac96daaa54435bde44
Madevelopment <mahir.delic.07@gmail.com>
2024-01-08 16:50:16 +0100

    added user functionality

---
+++ js/app.js
@@ -0,0 +1,43 @@
+
+let config = {
+    "korisnicko_ime": {
+        "required": true,
+        "minlength": 5,
+        "maxlength": 50
+    },
+    "register_email": {
+        "required": true,
+        "email": true,
+        "minlength": 5,
+        "maxlength": 50
+    },
+    "register_password": {
+        "required": true,
+        "minlength": 7,
+        "maxlength": 25,
+        "matching": "confirm_password"
+    },
+    "confirm_password": {
+        "required": true,
+        "minlength": 7,
+        "maxlength": 25,
+        "matching": "register_password"
+    }
+
+}
+
+let validator = new Validator(config, '#registrationForm');
+
+document.querySelector('#registrationForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if(validator.validationPassed()) {
+        let user = new User();
+        user.username = document.querySelector('#korisnicko_ime').value;
+        user.email = document.querySelector('#register_email').value;
+        user.password = document.querySelector('#register_password').value;
+        user.create();
+    } else {
+        alert('Polja nisu dobro popunjena');
+    }
+})
