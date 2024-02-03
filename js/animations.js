9ff20b61b0f61a9a08dde9490ee287bc5b5f2667
Madevelopment <mahir.delic.07@gmail.com>
2024-02-03 22:57:48 +0100

    improved animations

--- js/animations.js
+++ js/animations.js
@@ -1,77 +1,39 @@
-const tl = gsap.timeline({defaults: {duration: 0.35, ease: "Power2.easeOut"}})
-const home = document.querySelector(".home")
-const notifications = document.querySelector('.notifications')
-const messages = document.querySelector('.messages')
+const tl = gsap.timeline({defaults: {duration: 0.35, ease: "Power2.easeOut"}});
+const home = document.querySelector(".home");
+const notifications = document.querySelector('.notifications');
+const messages = document.querySelector('.messages');
+const homeSvg = document.querySelector('.home-svg');
+const feather = document.querySelector('.feather');
+const rightFeather = document.querySelector('.right-feather');
+const bell = document.querySelector('.bell');
+const ringer = document.querySelector('.ringer');
+const wave = document.querySelector('.wave');
+const messagesSvg = document.querySelector('.messages-svg');
+const flap = document.querySelector('.flap');
+const note = document.querySelector('.note');
 
-gsap.set('.feather', {scale: 0, transformOrigin: "center"})
-home.addEventListener('click', () => {
-    gsap.fromTo('.home-svg', {scale: 1}, {scale: 0.9, yoyp: true, repeat:1})
-    gsap.fromTo('.feather', {y:-5, scale:0}, {y:20, scale:1.5, duration: 1, stagger: 0.2})
-    gsap.fromTo('.right-feather', {x: 0}, {x: 5})
-})
+gsap.set('.feather', {scale: 0, transformOrigin: "center"});
 
-// Notification animation
-gsap.set('.ringer', {transformOrigin: "top center"})
-gsap.set('.bell', {transformOrigin: "top center"})
-gsap.set('.wave', {opacity: 0, transformOrigin: "bottom" })
-notifications.addEventListener('click', () => {
-    gsap.fromTo('.bell', {rotation: -5}, {rotation: 0, duration: 2, ease: "elastic.out(2, 0.2)"})
-    gsap.fromTo('.ringer', {rotation: -3, x:0.5}, {rotation: 0, x:0, duration: 1, ease: "elastic.out(2, 0.2)"})
-    gsap.fromTo('.wave', {scale: 0, opacity:1}, {scale: 1.3, opacity:0, duration:1})
-})
-
-
-// messages
-gsap.set('.flap', {transformOrigin: 'top'})
-messages.addEventListener('click', () => {
-    tl.fromTo('.messages-svg', {scale: 1}, {scale: 0.9})
-    tl.fromTo('.flap', {scale: 1}, {scale: -1}, '<30%')
-    tl.fromTo('.messages-svg', {scale: 0.9}, {scale: 1}, '<50%')
-    tl.fromTo('.note', {y:0, opacity:1}, {y: -40, opacity:0, duration:0.75})
-    tl.to('.flap', {scale:1}, "<50%")
-})
-  
-
-
-  
-function initAnimations() {
-    // Highlight page 2
-const tlH = gsap.timeline({  
-    scrollTrigger: {
-        trigger: '.section-text',
-        scrub: true,
-        start: "-40%",
-        end: "40%",
-    }
- })
-
- tlH.fromTo('.highlight', {color: "rgba(255, 255, 255, 0.4)"}, {color: "rgba(255, 255, 255, 1)", stagger: 1})
-
-
-const tlHRemove = gsap.timeline({  
-    scrollTrigger: {
-        trigger: '.section-text',
-        scrub: true,
-        start: "-20%",
-        end: "60%",
-    }
- });
-
- tlHRemove.to('.highlight', {color: "rgba(255, 255, 255, 0.4)", stagger: 1}) 
-
- const tl = gsap.timeline({ defaults: {duration: 0.75, ease: "power3.easeOut" } });
-
-tl.fromTo(".hero-img", { scale: 1.3, borderRadius: "0rem" }, { scale: 1, borderRadius: "2rem", delay: 0.35, duration: 2.5, ease:"elastic.out(1.5,1)" })
-tl.fromTo(".cta1", {x: "100%", color: "#ffc078", opacity: 0}, {x: 0, color: "#fff", opacity: 1}, "<10%")
-tl.fromTo(".cta2", {y: "100%",color: "#ffc078", opacity: 0}, {y: 0,color: "#fff", opacity: 1}, "<10%")
-tl.fromTo(".cta3", {x: "-100%",color: "#ffc078", opacity: 0}, {x: 0,color: "#fff", opacity: 1}, "<10%")
-
-
-
-
-
-
-
+function homeClickHandler() {
+    gsap.fromTo(homeSvg, {scale: 1}, {scale: 0.9, yoyp: true, repeat:1});
+    gsap.fromTo(feather, {y:-5, scale:0}, {y:20, scale:1.5, duration: 1, stagger: 0.2});
+    gsap.fromTo(rightFeather, {x: 0}, {x: 5});
 }
 
-initAnimations();
+function notificationsClickHandler() {
+    gsap.fromTo(bell, {rotation: -5}, {rotation: 0, duration: 2, ease: "elastic.out(2, 0.2)"});
+    gsap.fromTo(ringer, {rotation: -3, x:0.5}, {rotation: 0, x:0, duration: 1, ease: "elastic.out(2, 0.2)"});
+    gsap.fromTo(wave, {scale: 0, opacity:1}, {scale: 1.3, opacity:0, duration:1});
+}
+
+function messagesClickHandler() {
+    tl.fromTo(messagesSvg, {scale: 1}, {scale: 0.9});
+    tl.fromTo(flap, {scale: 1}, {scale: -1}, '<30%');
+    tl.fromTo(messagesSvg, {scale: 0.9}, {scale: 1}, '<50%');
+    tl.fromTo(note, {y:0, opacity:1}, {y: -40, opacity:0, duration:0.75});
+    tl.to(flap, {scale:1}, "<50%");
+}
+
+home.addEventListener('click', homeClickHandler);
+notifications.addEventListener('click', notificationsClickHandler);
+messages.addEventListener('click', messagesClickHandler);
